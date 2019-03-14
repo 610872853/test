cf23f2a14b08cfe0670bfbcae9076405b39c517c
yan <40845014+610872853@users.noreply.github.com>
2019-03-14 21:36:26 +0800

    Update ClientTest.java

--- src/cn/yan/netty/echo/ClientTest.java
+++ src/cn/yan/netty/echo/ClientTest.java
@@ -4,5 +4,6 @@ public class ClientTest {
 	public static void main(String[] args) throws Exception {
 		EchoClient client = new EchoClient("127.0.0.1", 10086);
 		client.run();
+		System.out.println("Test");
 	}
 }
